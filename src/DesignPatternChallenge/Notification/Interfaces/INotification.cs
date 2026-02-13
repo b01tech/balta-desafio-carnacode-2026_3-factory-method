a98b8717766af7a093ecbb171917b6e2e06bab49
b01tech <bruno01.tech@gmail.com>
2026-02-13 12:10:40 -0300

    feat(notification): add INotification interface

---
+++ src/DesignPatternChallenge/Notification/Interfaces/INotification.cs
@@ -0,0 +1,8 @@
+﻿namespace DesignPatternChallenge.Notification.Interfaces;
+
+public interface INotification
+{
+    void SendOrderConfirmation(string recipient, string orderNumber);
+    void SendShippingUpdate(string recipient, string trackingCode);
+    void SendPaymentReminder(string recipient, decimal amount);
+}
