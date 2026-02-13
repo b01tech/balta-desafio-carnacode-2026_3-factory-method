3991c6804c1b1fabceb65ff295bd35d062279e21
b01tech <bruno01.tech@gmail.com>
2026-02-13 12:10:52 -0300

    feat(notification): add ENotificationType enum

---
+++ src/DesignPatternChallenge/Notification/Factory/ENotificationType.cs
@@ -0,0 +1,10 @@
+namespace DesignPatternChallenge.Notification.Factory
+{
+    public enum ENotificationType
+    {
+        Email = 1,
+        WhatsApp = 2,
+        Push = 3,
+        SMS = 4,
+    }
+}
